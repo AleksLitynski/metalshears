cfb31099e3fec0c2c802c231605970b96f2884db
ajl <ajlitynski@gmail.com>
2024-06-23 23:54:22 -0400

    make a 'real' map and filled it with stuff (lots of new assets, so big commit)

--- game/main.gd
+++ game/main.gd
@@ -7,3 +7,8 @@ extends Node3D
 
 func _ready():
 	sound_manager.play_bgm(SoundManager.BGM.MAIN)
+	
+	var exterior_mesh : ArrayMesh = $arena_4/walls.mesh
+	var mat_0: StandardMaterial3D = exterior_mesh.surface_get_material(0)
+	mat_0.cull_mode = StandardMaterial3D.CULL_BACK
+	exterior_mesh.surface_set_material(0, mat_0)
---
+++ game/models/barber_pole/barber_pole.gd
@@ -0,0 +1,8 @@
+extends StaticBody3D
+
+
+func _ready():
+	return
+	# this animation looks like ass
+	$Sketchfab_Scene/AnimationPlayer.play("������Action_002")
+
